77938bb9a10673534c182f70dced73015361937e
Lewis-Murimi <lewismurimi195@gmail.com>
2025-09-06 13:56:48 +0300

    updated code

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,8 +1,8 @@
-from datetime import timezone
 
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils import timezone
 
 
 class Listing(models.Model):
